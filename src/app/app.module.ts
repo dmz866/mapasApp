21a5c007956b053930033053309d2be0a8abbd44
David Murillo <dmz866@hotmail.com>
2019-07-09 22:20:41 -0500

    map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { AgmCoreModule } from '@agm/core';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyAwSSgZJ4t8wwfS5U980B0WWhOMlCe40fE'})
   ],
   providers: [],
   bootstrap: [AppComponent]
